b046636e52ad47829c95ca20f256d52580107967
Laurin Berli <mail@laurinberli.ch>
2025-03-22 23:12:15 +0000

    fix - php & html

--- includes/openid-connect-generic-login-form.php
+++ includes/openid-connect-generic-login-form.php
@@ -150,12 +150,13 @@ class OpenID_Connect_Generic_Login_Form {
 		$href = $this->client_wrapper->get_authentication_url( $atts );
 		$href = esc_url_raw( $href );
 
-		$login_button = { ?>
+		ob_start();
+		?>
 			<div class="openid-connect-login-button" style="margin: 1em 0; text-align: center;">
 				<a class="button button-large" href="{$href}">{$text}</a>
 			</div>
-			<?php
-		}
+		<?php
+		$login_button = ob_get_clean();
 
 		return $login_button;
 	}
